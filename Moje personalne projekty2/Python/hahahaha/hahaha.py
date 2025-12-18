962608a15cf94ab9f728cc9330b08a79733952d3
6dziasty <kkssaaw85@gmail.com>
2025-12-18 17:29:24 +0100

    Delete Moje personalne projekty2 directory

--- Moje personalne projekty2/Python/hahahaha/hahaha.py
+++
@@ -1,4 +0,0 @@
-import cmd
-import os
-
-URL="https://www.youtube.com/watch?v"
--- Moje personalne projekty2/Python/projekty/obliczaniezl.py
+++
@@ -1,6 +0,0 @@
-iledni = int(input("Podaj liczbę dni: "))
-ilezl = int(input("Podaj liczbę złotych: "))
-
-koncowakwota = ilezl * iledni
-
-print(f"Po {iledni} dniach będziesz miał {koncowakwota} zł")
